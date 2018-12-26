b06a2b5d1ce729c6146de27535c5df11e88d5bc8
Andre Soares <andregsilv@gmail.com>
2018-12-26 17:40:59 -0200

    Add authentication middleware

--- SampleOidcClient.Mvc/Startup.cs
+++ SampleOidcClient.Mvc/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
 
 [assembly: OwinStartup(typeof(SampleOidcClient.Mvc.Startup))]
@@ -11,7 +14,25 @@ namespace SampleOidcClient.Mvc
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions()
+            {
+                ResponseType = "id_token",
+                UseTokenLifetime = false,
+                SignInAsAuthenticationType = CookieAuthenticationDefaults.AuthenticationType,
+
+                // Fill-in configuration below
+                Authority = "<authority>",
+                ClientId = "<client id>",
+                RedirectUri = "<redirect uri>",
+                Scope = "<scopes separated by space>"
+            });
         }
     }
 }
